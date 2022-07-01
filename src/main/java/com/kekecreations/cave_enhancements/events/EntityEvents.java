a6ca6795e109ce3c3fca70b9ca3010ebaa75b7fe
ExDrill <72329798+ExDrill@users.noreply.github.com>
2022-07-01 14:16:44 -0500

    Change Creeper scaring

--- src/main/java/com/kekecreations/cave_enhancements/events/EntityEvents.java
+++ src/main/java/com/kekecreations/cave_enhancements/events/EntityEvents.java
@@ -6,10 +6,15 @@ import com.kekecreations.cave_enhancements.entity.Cruncher;
 import com.kekecreations.cave_enhancements.entity.DripstonePike;
 import com.kekecreations.cave_enhancements.entity.DripstoneTortoise;
 import com.kekecreations.cave_enhancements.entity.Goop;
+import com.kekecreations.cave_enhancements.entity.ai.goal.FleeTheFluteGoal;
 import com.kekecreations.cave_enhancements.registry.ModEntities;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntitySelector;
 import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -26,5 +31,15 @@ public class EntityEvents {
         event.put(ModEntities.DRIPSTONE_TORTOISE.get(), DripstoneTortoise.createDripstoneTortoiseAttributes().build());
         event.put(ModEntities.DRIPSTONE_PIKE.get(), DripstonePike.createDripstonePikeAttributes().build());
         event.put(ModEntities.CRUNCHER.get(), Cruncher.createCruncherAttributes().build());
+    }
+
+    @SubscribeEvent
+    public static void onEntityJoin(EntityJoinWorldEvent event) {
+        Entity entity = event.getEntity();
+
+        if (entity instanceof Creeper creeper) {
+            creeper.goalSelector.addGoal(1, new FleeTheFluteGoal<>(creeper, EntitySelector.NO_CREATIVE_OR_SPECTATOR, 8.0F, 1.25D, 1.25D));
+        }
+
     }
 }
